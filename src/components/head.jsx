a7868a6c6c62e83a833038cc2f7bd4d0a61e1d2b
qais-max <usmanqais99@gmail.com>
2025-08-29 09:44:50 +0100

    update

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,16 +1,20 @@
+import logo from "../components/images/log.svg";
+
 export default function Head() {
   return (
     <div>
       <header className="bg-gray-200">
         <nav className="flex justify-between h-20 p-2 items-center">
-          <div className="lg  text-2xl">logo</div>
+          <div className="lg  text-2xl">
+            <img className="h-10 ml-5" src={logo} alt="" />
+          </div>
           <ul className="desk-nav hidden flex lg:flex text-1xl text-green-900 gap-20">
             <li>About</li>
             <li>Contact</li>
             <li>Portfolio</li>
           </ul>
           <div className="mobile-nav md:hidden  mb:block">
-            <div className="hamburger h-8 w-8 flex gap-2 flex-col mr-2 ">
+            <div className="hamburger h-8 w-8  flex gap-2 flex-col mr-2 ">
               <div className="bars h-1 w-8 bg-black "></div>
               <div className="bars h-1 w-8 bg-black "></div>
               <div className="bars h-1 w-8 bg-black"></div>
